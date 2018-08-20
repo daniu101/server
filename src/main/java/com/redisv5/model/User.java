5fad310e4078697debdcf60b889f955e0710b015
manqingxing <manqingxing@qq.com>
2018-08-20 13:43:16 +0800

    可以正式开发了

--- src/main/java/com/redisv5/model/User.java
+++ src/main/java/com/redisv5/model/User.java
@@ -1,51 +1,117 @@
 package com.redisv5.model;
 
-public class User {
-	private Integer id;
+import java.io.Serializable;
 
-	private String userName;
+public class User implements Serializable {
+    private Integer id;
 
-	private String password;
+    private String uId;
 
-	private Integer age;
+    private String name;
 
-	public Integer getId() {
-		return id;
-	}
+    private String password;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    private String email;
 
-	public String getUserName() {
-		return userName;
-	}
+    private String wechat;
 
-	public void setUserName(String userName) {
-		this.userName = userName == null ? null : userName.trim();
-	}
+    private String qq;
 
-	public String getPassword() {
-		return password;
-	}
+    private String position;
 
-	public void setPassword(String password) {
-		this.password = password == null ? null : password.trim();
-	}
+    private String company;
 
-	public Integer getAge() {
-		return age;
-	}
+    private Integer years;
 
-	public void setAge(Integer age) {
-		this.age = age;
-	}
+    private Integer salary;
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", userName=" + userName + ", password="
-				+ password + ", age=" + age + "]";
-	}
+    private static final long serialVersionUID = 1L;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId == null ? null : uId.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getWechat() {
+        return wechat;
+    }
+
+    public void setWechat(String wechat) {
+        this.wechat = wechat == null ? null : wechat.trim();
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq == null ? null : qq.trim();
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position == null ? null : position.trim();
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company == null ? null : company.trim();
+    }
+
+    public Integer getYears() {
+        return years;
+    }
+
+    public void setYears(Integer years) {
+        this.years = years;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
 }
--- src/main/java/com/redisv5/service/impl/UserServiceImpl.java
+++ src/main/java/com/redisv5/service/impl/UserServiceImpl.java
@@ -1,21 +1,20 @@
 package com.redisv5.service.impl;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
-import com.redisv5.dao.IUserDao;
+import com.redisv5.dao.UserMapper;
 import com.redisv5.model.User;
 import com.redisv5.service.IUserService;
-
 
 @Service("userService")
 public class UserServiceImpl implements IUserService {
 	@Resource
-	private IUserDao userDao;
+	private UserMapper userDao;
 
 	@Override
 	public User getUserById(int userId) {
-		// TODO Auto-generated method stub
 		return this.userDao.selectByPrimaryKey(userId);
 	}
 
